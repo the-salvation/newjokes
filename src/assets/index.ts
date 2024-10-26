41df70402e37b15958dcadb5b33f985100ee19d2
Eugene Markelov <eugene.m@ezlogz.com>
2024-10-26 17:37:08 +0300

    add fonts, icons, Header component

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as TodayIcon } from './icons/Today.svg';
+export { default as HistoryIcon } from './icons/History.svg';
+export { default as FavFilledIcon } from './icons/Fav-Filled.svg';
+export { default as FavOutlinedIcon } from './icons/Fav.svg';
