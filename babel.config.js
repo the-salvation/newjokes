55a1f1b162ead83ca6b90ff724e9a2c3e120cc02
Eugene Markelov <eugene.m@ezlogz.com>
2024-10-26 15:05:14 +0300

    enhance config for module resolution and add path aliases in babel and tsconfig files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@helpers': './src/utils/helpers',
+          '@hooks': './src/hooks',
+          '@navigator': './src/navigator',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@theme': './src/theme',
+          '@types': './src/types',
+          '@storage': './src/storage',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
+
